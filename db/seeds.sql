5b9b8c702b9cba73146beb6f8de7fe8ea110a193
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-03-02 22:49:28 -0800

    seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Mushroom Avocado Hamburger'),
+    ('Slammin Burger'),
+    ('Black Bean Burger'),
+    ('Hawaiian Burger'),
+    ('Buffalo Burger'),
+    ('Truffle Burger');
