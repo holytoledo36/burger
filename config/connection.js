19afb3637e6cdecfac1fc344eae4aea7677b5b69
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-03-04 13:29:35 -0800

    heroku

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,15 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+}
 
+else {
     connection = mysql.createConnection({
         host: "localhost",
+        port: 3306,
         user: "root",
-        password: "hacktheplanet",
-        database: "todoagain_db"
+        password: "",
+        database: "burgers_db"
     });
 }
 
